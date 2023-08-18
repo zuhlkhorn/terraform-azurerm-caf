e7401b5ae2428a471e6f90519cd20a2675b2b3ef
Sébastien Richoz <sebastien.richoz@zuehlke.com>
2023-08-18 06:47:16 +0000

    fix

--- modules/networking/express_route_connection/module.tf
+++ modules/networking/express_route_connection/module.tf
@@ -6,7 +6,7 @@ data "external" "express_route_circuit_peering_id" {
       "az network express-route peering list -resource_group '%s' --circuit-name '%s' --query '[?vlanId==`'%d'`].id' --output tsv",
       local.express_route_circuit.resource_group_name,
       local.express_route_circuit.name,
-      var.settings.express_route_circuit.vlan_id
+      var.settings.circuit_peering.vlan_id
     )
   ]
 }
