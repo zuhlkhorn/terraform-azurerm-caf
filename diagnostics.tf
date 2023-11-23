77a7302bf6d1c067eae1bce256a38f2d1c6271fd
Sébastien Richoz <sebastien.richoz@zuehlke.com>
2023-11-23 15:45:07 +0000

    fix missing var ref

--- diagnostics.tf
+++ diagnostics.tf
@@ -80,6 +80,7 @@ module "diagnostic_log_analytics" {
 
   global_settings = local.global_settings
   log_analytics   = each.value
+  tables          = try(each.value.tables, {})
 
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
--- log_analytics.tf
+++ log_analytics.tf
@@ -5,7 +5,7 @@ module "log_analytics" {
 
   global_settings = local.global_settings
   log_analytics   = each.value
-  tables          = each.value.tables
+  tables          = try(each.value.tables, {})
   resource_group  = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
   base_tags       = local.global_settings.inherit_tags
 }
