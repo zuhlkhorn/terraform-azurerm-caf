88746209b05f0a27e7ef65026267fd2eee379ef9
Sébastien Richoz <sebastien.richoz@zuehlke.com>
2023-11-23 15:16:25 +0000

    log analytics workspace table update retention days

---
+++ modules/log_analytics/table_retention.tf
@@ -0,0 +1,13 @@
+resource "terraform_data" "update_log_analytics_workspace_tables" {
+  for_each = var.tables
+
+  triggers_replace = [
+    table_name = each.value.name
+    retention_in_days = each.value.retention_in_days
+    total_retention_in_days = each.value.total_retention_in_days
+  ]
+
+  provisioner "local-exec" {
+    command = "az monitor log-analytics workspace table update --resource-group ${local.resource_group_name} --workspace-name ${azurerm_log_analytics_workspace.law.name} --name ${each.value.name} --retention-time ${each.value.retention_in_days} --total-retention-time ${each-value.total_retention_in_days}"
+  }
+}
--- modules/log_analytics/variables.tf
+++ modules/log_analytics/variables.tf
@@ -15,6 +15,9 @@ variable "resource_group_name" {
 variable "resource_group" {
   description = "Resource group object to deploy the virtual machine"
 }
+variable "tables" {
+  description = "table configuration for retention days"
+}
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = bool
