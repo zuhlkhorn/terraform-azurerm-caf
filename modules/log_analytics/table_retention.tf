64641b57673d984ff495af0364374b0f0d67cd7b
Sébastien Richoz <sebastien.richoz@zuehlke.com>
2023-11-23 15:48:41 +0000

    fix

--- modules/log_analytics/table_retention.tf
+++ modules/log_analytics/table_retention.tf
@@ -9,6 +9,6 @@ resource "terraform_data" "update_log_analytics_workspace_tables" {
   ]
 
   provisioner "local-exec" {
-    command = "az monitor log-analytics workspace table update --resource-group ${local.resource_group_name} --workspace-name ${azurerm_log_analytics_workspace.law.name} --name ${each.value.name} --retention-time ${each.value.retention_in_days} --total-retention-time ${each-value.total_retention_in_days}"
+    command = "az monitor log-analytics workspace table update --resource-group ${local.resource_group_name} --workspace-name ${azurerm_log_analytics_workspace.law.name} --name ${each.value.name} --retention-time ${each.value.retention_in_days} --total-retention-time ${each.value.total_retention_in_days}"
   }
 }
