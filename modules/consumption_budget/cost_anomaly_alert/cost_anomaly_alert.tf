19fcb7dd86cd5ea13ee646e7a1f9b2f0b02b0d98
Dany Löffler <dany.loeffler@zuehlke.com>
2023-12-12 11:06:56 +0100

    Added cost anomaly alerts

---
+++ modules/consumption_budget/cost_anomaly_alert/cost_anomaly_alert.tf
@@ -0,0 +1,16 @@
+resource "azurecaf_name" "this_name" {
+  name          = var.settings.name
+  prefixes      = var.global_settings.prefixes
+  resource_type = "azurerm_cost_anomaly_alert"
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
+
+resource "azurerm_cost_anomaly_alert" "this" {
+  name            = azurecaf_name.this_name.result
+  display_name    = var.settings.display_name
+  email_subject   = var.settings.email_subject
+  email_addresses = var.settings.email_addresses
+}
---
+++ modules/consumption_budget/cost_anomaly_alert/output.tf
@@ -0,0 +1,4 @@
+output "id" {
+  description = "The ID of the cost anomaly alert"
+  value       = azurerm_cost_anomaly_alert.this.id
+}
