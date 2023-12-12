6217dd99d99e46442615249840fe6578a8cbed84
Dany Löffler <dany.loeffler@zuehlke.com>
2023-12-12 12:40:33 +0100

    Fix

--- modules/consumption_budget/cost_anomaly_alert/cost_anomaly_alert.tf
+++ modules/consumption_budget/cost_anomaly_alert/cost_anomaly_alert.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cost_anomaly_alert" "this" {
-  name            = azuvar.settings.name
+  name            = var.settings.name
   display_name    = var.settings.display_name
   email_subject   = var.settings.email_subject
   email_addresses = var.settings.email_addresses
