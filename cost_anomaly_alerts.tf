d3c26b3430c31dad64cd880cc1e3c50798473249
Sébastien Richoz <sebastien.richoz@zuehlke.com>
2024-01-25 07:24:19 +0000

    cost anomaly alert must be created fter budget exists

--- cost_anomaly_alerts.tf
+++ cost_anomaly_alerts.tf
@@ -1,6 +1,6 @@
 module "cost_anomaly_alerts" {
   source = "./modules/consumption_budget/cost_anomaly_alert"
-  depends_on = [ module.subscriptions ]
+  depends_on = [ module.subscriptions, module.consumption_budgets_subscriptions, module.consumption_budgets_resource_groups ]
   for_each = try(local.shared_services.cost_anomaly_alerts, {})
 
   client_config   = local.client_config
