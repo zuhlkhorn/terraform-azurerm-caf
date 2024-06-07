7d96bf9053dfa90685bf9955120e3e18491f7c36
Luka Lodrant <luka.lodrant@zuehlke.com>
2024-06-07 13:12:42 +0000

    fix custom role assignments on mgs

--- modules/roles/custom_roles/output.tf
+++ modules/roles/custom_roles/output.tf
@@ -7,3 +7,8 @@ output "role_definition_resource_id" {
   value = azurerm_role_definition.custom_role.role_definition_resource_id
 
 }
+
+output "role_definition_name" {
+  value = azurecaf_name.custom_role.result
+
+}
